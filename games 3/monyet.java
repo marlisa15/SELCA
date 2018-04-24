108627395ab6c599a6f519e27f61c051d5e02d8b
DESKTOP-5FCF0RL\Marlisa <ichamarlisa15@gmail.com>
2018-04-24 13:41:07 +0700

    pertama

---
+++ games 3/monyet.java
@@ -0,0 +1,45 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class monyet here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class monyet extends benda
+{
+    /**
+     * Act - do whatever the kereta wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        {
+         moveMonyet(); //method moveMonkey
+         objectDisappear(); //method objectDisappear
+        }
+    }    
+       
+    public void objectDisappear() //method objectDisappear
+    {
+         if (canSee(pisang.class)) //bila melihat kelas objek banana
+         {
+             eat(pisang.class); //eat kelas objek banana
+             ((Counter)getWorld().getObjects(Counter.class).get(0)).Counting(10); // kelas counter ditambah sebanyak 10 poin
+             Greenfoot.playSound("score.wav"); // mainkan sound score.wav
+         }
+    }
+       
+    public void moveMonyet() //method moveMonkey
+     {
+         if (Greenfoot.isKeyDown("left")) //bila arah panah ke kiri ditekan
+         {
+             move(-7); //bergerak ke -7
+         }
+         if (Greenfoot.isKeyDown("right")) //bila arah panah ke kanan ditekan
+         {
+             move(7); //bergerak ke 7
+         }
+     }
+}
+
---
+++ games 3/pisang.java
@@ -0,0 +1,30 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class pisang here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class pisang extends benda
+{
+    /**
+     * Act - do whatever the lemon wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    private int timer=0;
+    public void act() 
+    {
+        setLocation(getX(), getY()+6); //posisi objek banana, kecepatan banana = +6
+        PisangFall(); //lakukan method BananaFall
+    } 
+    
+    public void  PisangFall() //method BananaFall
+     {
+         if (atWorldEdge()) //jika objek banana berada di bawah world
+         {
+             getWorld().removeObject(this); // remove objek banana
+             Greenfoot.playSound("pisangfall.wav"); // mainkan sound bananafall.wav
+         }
+     }
+}
