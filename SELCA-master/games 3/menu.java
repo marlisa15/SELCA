9e3d825a4328b6f1fef7f55a5e9a32ca7767e51a
whirlwind\se_sel <seselvia93@gmail.com>
2018-05-18 10:54:21 +0700

    ketujuh

---
+++ SELCA-master/games 3/menu.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class menu here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class menu extends World
+{
+
+    /**
+     * Constructor for objects of class menu.
+     * 
+     */
+    public menu()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        
+        addObject (new play(), 515,350);
+        addObject (new exit(), 85,350);
+    }
+}
