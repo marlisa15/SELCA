9e1f220933a883b22cbdd62b2ee57b53937622d4
DESKTOP-5FCF0RL\Marlisa <ichamarlisa15@gmail.com>
2018-05-20 23:13:23 +0700

    14

--- SELCA-master/games 3/menu.java
+++ SELCA-master/games 3/menu.java
@@ -6,19 +6,18 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class menu extends World
+public class Menu extends World
 {
 
     /**
      * Constructor for objects of class Menu.
      * 
      */
-    public menu()
+    public Menu()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
         
         addObject (new mulai(), 515,350);
-        addObject (new exit(), 97,350);
     }
 }
